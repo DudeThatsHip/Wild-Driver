29b6490bc492dd3318f4af41ad6d34026d8c393d
DudeThatsHip <86862723+DudeThatsHip@users.noreply.github.com>
2021-12-20 11:47:39 -0500

    Added comment to driver script and messing with physics

--- Wild Driver/Assets/Scripts/Driver.cs
+++ Wild Driver/Assets/Scripts/Driver.cs
@@ -18,9 +18,9 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Vehicle Movement, rotation and y-axis
         float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
     }
